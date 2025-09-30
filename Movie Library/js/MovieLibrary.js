bd556f8c4ada74604431d8bf0bb508d44fbb121f
Laravaa <251111@virtualwindow.co.za>
2025-09-30 13:09:11 +0200

    Bootstrap integration
    Preparing to include bootstrap

--- Movie Library/js/MovieLibrary.js
+++ Movie Library/js/MovieLibrary.js
@@ -8,31 +8,37 @@ const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
 
 //Fetching popular movies on load
-document.addEventListener("DOMContentLoaded" ,() => {
-    fetchMovies(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+document.addEventListener("DOMContentLoaded", () => {
+  fetchMovies(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+  );
 });
 
 //The fetch movies function
 async function fetchMovies(url) {
-    try{
-        const res = await fetch (url);
-        const data = await res.json();
-        displayMovies(data.results);
-    } catch (error) {
-        console.error("Error fetching movies: ", error);
-    }
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMovies(data.results);
+  } catch (error) {
+    console.error("Error fetching movies: ", error);
+  }
 }
 
 //Displaying movies on the grid
 function displayMovies(movies) {
   movieGrid.innerHTML = "";
 
-  movies.forEach(movie => {
+  movies.forEach((movie) => {
     const card = document.createElement("div");
     card.classList.add("movie-card");
 
     card.innerHTML = `
-      <img src="${movie.poster_path ? IMG_BASE_URL + movie.poster_path : 'assets/placeholder.jpg'}" alt="${movie.title}">
+      <img src="${
+        movie.poster_path
+          ? IMG_BASE_URL + movie.poster_path
+          : "assets/placeholder.jpg"
+      }" alt="${movie.title}">
       <h3>${movie.title}</h3>
       <button onclick="addToWatchlist('${movie.id}')">✔ Add</button>
     `; // fixed with backticks
@@ -43,13 +49,13 @@ function displayMovies(movies) {
 
 //Search functionality
 searchBtn.addEventListener("click", () => {
-    const query = searchInput.ariaValueMax.trim();
-    if (query) {
-        fetchMovies(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
-    }
+  const query = searchInput.value.trim();
+  if (query) {
+    fetchMovies(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
+  }
 });
 
 //Placeholder Watchlist
 function addToWatchlist(movieId) {
-    alert(`Movie ${movieId} added to watchlist!`);
+  alert(`Movie ${movieId} added to watchlist!`);
 }
