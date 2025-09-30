df8b700146c01b8ca6b6f49f5a28195592087d68
Laravaa <251111@virtualwindow.co.za>
2025-09-30 17:10:46 +0200

    Oops
    Fixing a js mistake

--- Movie Library/js/MovieLibrary.js
+++ Movie Library/js/MovieLibrary.js
@@ -1,4 +1,4 @@
-//Setting up the TMDB API
+b//Setting up the TMDB API
 const API_KEY = "0d4ce6a4966a08401c202627e29b935a";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
@@ -10,16 +10,18 @@ const searchInput = document.getElementById("searchInput");
 //Fetching popular movies on load
 document.addEventListener("DOMContentLoaded", () => {
   fetchMovies(
-    `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`, 25
   );
 });
 
 //The fetch movies function
-async function fetchMovies(url) {
+async function fetchMovies(url, limit = 20) {
   try {
     const res = await fetch(url);
     const data = await res.json();
-    displayMovies(data.results);
+
+    const movies = data.results.slice(0, limit);
+    displayMovies(movies);
   } catch (error) {
     console.error("Error fetching movies: ", error);
   }
@@ -30,20 +32,22 @@ function displayMovies(movies) {
   movieGrid.innerHTML = "";
 
   movies.forEach((movie) => {
-    const card = document.createElement("div");
-    card.classList.add("movie-card");
+    const col = document.createElement("div");
+    col.classList.add("col");
 
-    card.innerHTML = `
-      <img src="${
+    col.innerHTML = `
+     <div class = "movie-card">
+     <img src="${
         movie.poster_path
-          ? IMG_BASE_URL + movie.poster_path
-          : "assets/placeholder.jpg"
-      }" alt="${movie.title}">
-      <h3>${movie.title}</h3>
-      <button onclick="addToWatchlist('${movie.id}')">✔ Add</button>
-    `; // fixed with backticks
+        ? IMG_BASE_URL + movie.poster_path
+        : "/Movie\ Library/assets/placeholder.jpg"}
+        " alt="${movie.title}">
+        <h3${movie.title}</h3>
+        <button onclick="addToWatchlist('${movie.id}'>Add</button>
+        </div>
+    `; 
 
-    movieGrid.appendChild(card);
+    movieGrid.appendChild(col);
   });
 }
 
@@ -51,11 +55,11 @@ function displayMovies(movies) {
 searchBtn.addEventListener("click", () => {
   const query = searchInput.value.trim();
   if (query) {
-    fetchMovies(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
+    fetchMovies(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`, 25);
   }
 });
 
 //Placeholder Watchlist
 function addToWatchlist(movieId) {
-  alert(`Movie ${movieId} added to watchlist!`);
+  alert(`Movie ${movieId} added to Watchlist!`);
 }
