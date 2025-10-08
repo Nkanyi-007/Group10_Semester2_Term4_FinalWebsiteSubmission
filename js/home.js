36415c985a826d6696d6413f4fbca1e4a34e8e71
RhichelleStrauss <251169@virtualwindow.co.za>
2025-10-08 11:51:37 +0200

    creation of function to display movies dynamically
    created the function that allows the dynamic display of recently released movies from API

--- js/home.js
+++ js/home.js
@@ -9,6 +9,63 @@ class Movie {
     }
 }
 
+// function isHighlyRated(movie){
+//     return parseInt(movie.rating) >= 7;
+// }
+
+function displayMovies(moviesArray) {
+    const movieHeaderContainer = document.getElementById('movieHeader');
+
+    movieHeaderContainer.innerHTML = '';
+
+    let movieSlider = `<div id="carouselExample" class="carousel slide" >
+  <div class="carousel-inner">`;
+
+    //map store 2 params, movie - movies data, index is the index in the array - 0,1,2 etc
+    // {} using that in arrow func - add more processing into func then specify what func actually guives back w return
+    // by using ternary ops can assign active only to first item in caresoul, index in array
+
+    movieSlider += moviesArray.map((movie, index) => {
+        const active = index == 0 ? 'active' : '';
+        //index - only first item in array get active
+
+        return (
+        `
+        <div class="carousel-item ${active}">
+        <div class="row align-items-center">
+
+
+            <div class="col-md-5">
+                <img src="${movie.image}" class="d-block w-100" alt="${movie.title}">
+            </div>
+
+
+            <div class="col-md-7 text-start p-4">
+                <h2 class="movie-title">${movie.title}</h2>
+                <p class="movie-rating"><strong>Rating:</strong> ${movie.rating.toFixed(1)} </p>
+                <p class="release-date"><strong>Released:</strong> ${movie.releaseDate}</p>
+                <p class="movie-overview">${movie.overview}</p>
+            </div>
+
+
+        </div>
+    </div>
+    `)
+
+    }).join(''); //converts to 1 string
+    movieSlider += `</div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>`;
+    movieHeaderContainer.innerHTML = movieSlider;
+}
+
 !async function () {
 
     const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
@@ -32,3 +89,4 @@ class Movie {
     };
 
 }();
+
