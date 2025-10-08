bdc48a9375bef87b49407d92d961082282b09d13
RhichelleStrauss <251169@virtualwindow.co.za>
2025-10-08 11:17:09 +0200

    basic file setup
    setting up html, js and css files

---
+++ js/home.js
@@ -0,0 +1,11 @@
+class Movie{
+    constructor(image, title, releaseDate, rating, overview, popularity){
+        this.image = image;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.rating = rating;
+        this.overview = overview;
+        this.popularity = popularity;
+    }
+}
+
